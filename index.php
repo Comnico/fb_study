f86ab76e70a4dae6d90d3f570585bcd1e07ec352
Masaru Furuya <m.furuya@comnico.jp>
2015-06-13 20:25:13 +0900

    Feed一覧画面コーディング

--- index.php
+++ index.php
@@ -1,22 +1,13 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
   <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="fb_study.css">
+    <link rel="stylesheet" type="text/css" href="index.css">
     <title>FB Study</title>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
   <body>
   <div class="header">
-    <a href="top.html" class="brand">FB Study</a>
+    <a href="index.php" class="brand">FB Study</a>
   </div>
   <div class="contents">
     <div class="login-area">
--- list.php
+++ list.php
@@ -9,5 +9,88 @@ if (empty($_SESSION['access_token'])) {
   exit();
 }
 ?>
-<p>ログイン中です</p>
-<a href="logout.php">ログアウトする</a>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="list.css">
+    <title>FB Study</title>
+  </head>
+  <body>
+    <div class="header">
+      <a class="logo" href="index.php" class="brand">FB Study</a>
+      <a class="logout" href="logout.php">ログアウトする</a>
+    </div>
+    <div class="contents">
+      <ul>
+        <li class="fb-post">
+          <div class="post-header">
+            <img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xaf1/v/t1.0-1/p50x50/1779052_612208972192374_84058029_n.jpg?oh=ad4ab9ed62e3374e7c286fb778296f3f&oe=56341A59&__gda__=1442133044_4edb287f6c30d67a8d6fbcbf122c8f25">
+            <div class="user-name" style="display:inline-block">
+              <a href="#">古家 大</a>
+              <p class="post-date">1時間前</p>
+            </div>
+          </div>
+          <div class="post-content">
+            <p class="content-body">良記事。iterm2は使ってみたい。</p>
+            <a href="#"><img src="https://fbexternal-a.akamaihd.net/safe_image.php?d=AQDZmQwaMJjhBZip&w=487&h=255&url=http%3A%2F%2Fmae.chab.in%2Fwp-content%2Fuploads%2F2015%2F05%2Fatom.png&cfs=1&upscale=1"></a>
+          </div>
+        </li>
+        <li class="fb-post">
+          <div class="post-header">
+            <img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xaf1/v/t1.0-1/p50x50/1779052_612208972192374_84058029_n.jpg?oh=ad4ab9ed62e3374e7c286fb778296f3f&oe=56341A59&__gda__=1442133044_4edb287f6c30d67a8d6fbcbf122c8f25">
+            <div class="user-name" style="display:inline-block">
+              <a href="#">古家 大</a>
+              <p class="post-date">1時間前</p>
+            </div>
+          </div>
+          <div class="post-content">
+            <p class="content-body">良記事。iterm2は使ってみたい。</p>
+            <a href="#"><img src="https://fbexternal-a.akamaihd.net/safe_image.php?d=AQDZmQwaMJjhBZip&w=487&h=255&url=http%3A%2F%2Fmae.chab.in%2Fwp-content%2Fuploads%2F2015%2F05%2Fatom.png&cfs=1&upscale=1"></a>
+          </div>
+        </li>
+        <li class="fb-post">
+          <div class="post-header">
+            <img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xaf1/v/t1.0-1/p50x50/1779052_612208972192374_84058029_n.jpg?oh=ad4ab9ed62e3374e7c286fb778296f3f&oe=56341A59&__gda__=1442133044_4edb287f6c30d67a8d6fbcbf122c8f25">
+            <div class="user-name" style="display:inline-block">
+              <a href="#">古家 大</a>
+              <p class="post-date">1時間前</p>
+            </div>
+          </div>
+          <div class="post-content">
+            <p class="content-body">良記事。iterm2は使ってみたい。</p>
+            <a href="#"><img src="https://fbexternal-a.akamaihd.net/safe_image.php?d=AQDZmQwaMJjhBZip&w=487&h=255&url=http%3A%2F%2Fmae.chab.in%2Fwp-content%2Fuploads%2F2015%2F05%2Fatom.png&cfs=1&upscale=1"></a>
+          </div>
+        </li>
+        <li class="fb-post">
+          <div class="post-header">
+            <img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xaf1/v/t1.0-1/p50x50/1779052_612208972192374_84058029_n.jpg?oh=ad4ab9ed62e3374e7c286fb778296f3f&oe=56341A59&__gda__=1442133044_4edb287f6c30d67a8d6fbcbf122c8f25">
+            <div class="user-name" style="display:inline-block">
+              <a href="#">古家 大</a>
+              <p class="post-date">1時間前</p>
+            </div>
+          </div>
+          <div class="post-content">
+            <p class="content-body">良記事。iterm2は使ってみたい。</p>
+            <a href="#"><img src="https://fbexternal-a.akamaihd.net/safe_image.php?d=AQDZmQwaMJjhBZip&w=487&h=255&url=http%3A%2F%2Fmae.chab.in%2Fwp-content%2Fuploads%2F2015%2F05%2Fatom.png&cfs=1&upscale=1"></a>
+          </div>
+        </li>
+        <li class="fb-post">
+          <div class="post-header">
+            <img src="https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xaf1/v/t1.0-1/p50x50/1779052_612208972192374_84058029_n.jpg?oh=ad4ab9ed62e3374e7c286fb778296f3f&oe=56341A59&__gda__=1442133044_4edb287f6c30d67a8d6fbcbf122c8f25">
+            <div class="user-name" style="display:inline-block">
+              <a href="#">古家 大</a>
+              <p class="post-date">1時間前</p>
+            </div>
+          </div>
+          <div class="post-content">
+            <p class="content-body">良記事。iterm2は使ってみたい。</p>
+            <a href="#"><img src="https://fbexternal-a.akamaihd.net/safe_image.php?d=AQDZmQwaMJjhBZip&w=487&h=255&url=http%3A%2F%2Fmae.chab.in%2Fwp-content%2Fuploads%2F2015%2F05%2Fatom.png&cfs=1&upscale=1"></a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </body>
+</html>
+
